5d5696f015d8d60752e93d89045f2e0dc91e750c
Jack Jansen <Jack.Jansen@cwi.nl>
2025-05-12 12:45:29 +0200

    get_dash_sfu now also has argument to give the version you want to download.

--- external-packages/get_dash_sfu.sh
+++ external-packages/get_dash_sfu.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-
+case x$1 in
+x)
+	echo Usage: $0 release
+	echo Will download binaries from https://github.com/MotionSpell/lldash/releases/download/release
+	echo and install them in the correct place here
+	exit 1
+	;;
+x*)
+	release=$1
+	;;
+esac
 set -x
 mkdir -p tmp
 
-distr=lldash-linux-x86_64-0.9.3
+distr=lldash-linux-x86_64-${release}
 
-curl --location --output tmp/$distr.tar.gz https://github.com/cwi-dis/lldash/releases/download/v0.9.3/$distr.tar.gz
-# https://github.com/cwi-dis/VR2G-Evanescent/releases/download/v7.1.1_stable/evanescent.tar.bz2
+curl --location --output tmp/$distr.tar.gz https://github.com/MotionSpell/lldash/releases/download/${release}/${distr}.tar.gz
 rm -rf tmp/$distr
 (cd tmp && tar xfv $distr.tar.gz)
 rm -rf ../packages/lldash
